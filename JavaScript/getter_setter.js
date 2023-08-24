669ddfc43b015e8c7c3be51237ffe15efe9e2153
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-08-24 20:58:06 +0530

    Getter and Setter function in JS

---
+++ JavaScript/getter_setter.js
@@ -0,0 +1,14 @@
+let obj ={
+    age:30,
+    get a()
+    {
+        return this.age;
+    },
+    set b(newage)
+    {
+        this.age=newage;
+    }
+}
+console.log(obj.a);
+obj.b=40;
+console.log(obj.a);
