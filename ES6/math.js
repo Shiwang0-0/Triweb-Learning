d7a9cab625a6aec4bb274210487ac9a9e3da03a3
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-08-28 23:40:54 +0530

    Math functions and object in ES6

---
+++ ES6/math.js
@@ -0,0 +1,24 @@
+//math.trunc(returns integer)
+
+console.log(Math.trunc(5));
+console.log(Math.trunc(5.0)); 
+console.log(Math.trunc(5.34)); 
+console.log(Math.trunc(5.5));
+console.log(Math.trunc(5.7));
+console.log(Math.trunc(-5.37)); // -5
+//all +5  ,deletes the floating part and only returns interger.
+
+
+
+console.log(Math.sign(-5)); //-1
+console.log(Math.sign(5)); //1
+console.log(Math.sign(0)); //0
+
+
+
+//cube root
+console.log(Math.cbrt(64));
+
+console.log(Math.log2(8));
+console.log(Math.log10(100));
+
