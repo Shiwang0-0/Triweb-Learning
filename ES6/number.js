f96079a61caf4bcd9be513d47392111fe60dbae0
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-08-28 23:42:00 +0530

    Number and Number method in ES6

---
+++ ES6/number.js
@@ -0,0 +1,19 @@
+//epsilon
+
+let x=Number.EPSILON;
+console.log(x);
+
+let y=Number.MAX_SAFE_INTEGER;
+console.log(y);
+
+let z=Number.MIN_SAFE_INTEGER;
+console.log(z);
+
+
+
+//number methods
+
+let a=123;
+let b=3748092749872380789370598745;
+console.log(Number.isSafeInteger(a)); //true
+console.log(Number.isSafeInteger(b)); //false
