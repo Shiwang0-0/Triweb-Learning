a8894de769a2200fb3d2cdc0c49ebe858de792ce
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-09-23 17:58:37 +0530

    Product route added

--- React/myproject/src/App.js
+++ React/myproject/src/App.js
@@ -1,17 +1,19 @@
 import {Routes,Route} from 'react-router-dom';
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Product from './pages/Product';
 import Contact from "./pages/Contact";
+import Layout from './components/Layouts/Layout';
 function App() {
   return (
-    <div >
+   <Layout>
       <Routes>
       <Route path='/' element={<Home />}></Route>
       <Route path='/about' element={<About />}></Route>
       <Route path='/contact' element={<Contact />}></Route>
+      <Route path='/Product' element={<Product />}></Route>
       </Routes>
-      
-    </div>
+    </Layout>
   );
 }
 
