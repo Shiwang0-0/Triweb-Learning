2d5d9f53225481a30ed930dc157a47d34239752f
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-09-28 13:45:16 +0530

    added context for favourite products

--- React/myproject/src/components/product/ProductItem.js
+++ React/myproject/src/components/product/ProductItem.js
@@ -1,8 +1,29 @@
 import Styles from './ProductItem.module.css'
 import CustomWrapper from '../Layouts/CustomWrapper';
+import FavouriteContext from '../../store/ContextFavourite';
+import { useContext } from 'react';
+
+
 function ProductItem(props)
 {
-     return <li key={props.item.id}>
+    const favouriteContext=useContext(FavouriteContext);
+
+    const isFavourite=favouriteContext.isFavouriteItem(props.item._id);
+            
+    const toggleFavourite=()=>{
+        if(isFavourite)
+        {
+            favouriteContext.removeFavouriteItem(props.item._id);
+            // console.log("removed");
+        }
+        else
+        {
+            favouriteContext.addFavouriteItem({...props.item}); //normally the reference of the object will go,using spread operator can help sending the actual object.
+            // console.log("added");
+        }
+    }
+
+     return <li key={props.item._id}>
         <CustomWrapper>
             <div className={Styles.image}>
             <img src={props.item.image} alt="NO" />
@@ -14,6 +35,9 @@ function ProductItem(props)
         </div>
         <div className={Styles.btn}>
             <button >Buy</button>
+        </div>
+        <div>
+        <button className={isFavourite?Styles.heartFvrt:Styles.heartUnfvrt}onClick={toggleFavourite} ></button>
         </div>
         </CustomWrapper>
         
