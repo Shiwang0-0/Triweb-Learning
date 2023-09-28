3bbc3df5a8b8776f118e0ad32c47957088640300
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-09-28 13:41:36 +0530

    added favourite items

--- React/myproject/src/components/Layouts/MainMenu.js
+++ React/myproject/src/components/Layouts/MainMenu.js
@@ -1,8 +1,11 @@
 import { Link } from 'react-router-dom';
 import MainMenuStyle from './MainMenu.module.css';
-
+import FavouriteContext from '../../store/ContextFavourite';
+import { useContext } from 'react';
 
 function MainMenu() {
+    const favouriteContext=useContext(FavouriteContext);
+    const totalFavouriteItems=favouriteContext.favouriteItems.length;
     return <header className={MainMenuStyle.header}>
             <ul>
                 <li><Link to="/">Home</Link></li>
@@ -10,6 +13,7 @@ function MainMenu() {
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/product">Product</Link></li>
                 <li><Link to="/product/add">Add Product</Link></li>
+                <li className={MainMenuStyle.favouriteCount}><Link to="/product/favourite">{totalFavouriteItems}</Link></li>
                
             </ul>
     </header>
--- React/myproject/src/index.js
+++ React/myproject/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import {BrowserRouter} from 'react-router-dom';
-
+import { BrowserRouter } from 'react-router-dom';
+import { ContextFavourite } from './store/ContextFavourite';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter> <App /> </BrowserRouter>
+  <ContextFavourite>
+    <BrowserRouter> <App /> </BrowserRouter>
+  </ContextFavourite>
+
 );
 
