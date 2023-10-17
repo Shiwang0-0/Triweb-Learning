f538e090861779db3db968ea3cd3c7298fbade83
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-17 14:48:24 +0530

    images for skill section added

--- Projects/portfolio/src/components/skills/skills.jsx
+++ Projects/portfolio/src/components/skills/skills.jsx
@@ -1,21 +1,32 @@
 import './skills.css'
-import logo from "../../assets/logo.png"
+import js from "../../assets/JS.png"
+import html from "../../assets/html2.png"
+import css from "../../assets/css.png"
+import cpp from "../../assets/c++.png"
+import c from "../../assets/C.png"
+import react from "../../assets/react.png"
+import node from "../../assets/node.png"
+import mongo from "../../assets/mongo3.png"
 export const Skills=()=>{
     return(
-        <div className='skillsText'>
-            Skills
+        <div>
+            <div className='skillsText'>Skills</div>
         <div className="skillsSection">
             <div className='skillsSectionBox'>
-            <img src={logo} alt="Logo" className="skill"/>
-            <img src={logo} alt="Logo" className="skill"/>
-            <img src={logo} alt="Logo" className="skill"/>
+            <img src={js} alt="Logo" className="skill"/>
+            <img src={html} alt="Logo" className="skill"/>
+            <img src={css} alt="Logo" className="skill"/>
             <br/>
-            <img src={logo} alt="Logo" className="skill"/>
-            <img src={logo} alt="Logo" className="skill"/>
-            <img src={logo} alt="Logo" className="skill"/>
+            <img src={cpp} alt="Logo" className="skill"/>
+            <img src={c} alt="Logo" className="skill"/>
+            <img src={react} alt="Logo" className="skill"/>
+            <img src={mongo} alt="Logo" className="skill"/>
+            <img src={node} alt="Logo" className="skill"/>
             </div>    
+        
         </div>
         </div>
+        
     )
     
 }
