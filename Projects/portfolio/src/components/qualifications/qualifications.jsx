01aff16b53c03e1702140c6dc2062fd1b534c0b8
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-17 14:45:20 +0530

    added school and college logo

--- Projects/portfolio/src/components/qualifications/qualifications.jsx
+++ Projects/portfolio/src/components/qualifications/qualifications.jsx
@@ -1,5 +1,6 @@
 import './qualifications.css'
-import logo from "../../assets/logo.png"
+import geu from "../../assets/geu.jpg"
+import dgs from "../../assets/dgs.png"
 export const Qualifications=()=>{
     return(
         <div>
@@ -11,21 +12,21 @@ export const Qualifications=()=>{
             <div className='Quali'>
                 <div className='courseHeading'>BTech CSE</div>
                 <div className='college'>
-                <img src={logo} alt="collegeLogo" className="collegeLogo"/>
+                <img src={geu} alt="collegeLogo" className="collegeLogo"/>
                 <div className='collegeData'>Graphic Era Deemed to be University (2022-present)<br/>Iam pursuing my BTech in Computer Science and Enginnering Branch.</div>
                 </div>
              </div>
              <div className='Quali'>
              <div className='courseHeading'>Intermediate</div>
                 <div className='college'>
-                <img src={logo} alt="collegeLogo" className="collegeLogo"/>
+                <img src={dgs} alt="collegeLogo" className="collegeLogo"/>
                 <div className='collegeData'>Doon Global School (2021-22)<br/>I completed my +2 studies in 2022 with PCM as core subjects  and got 84%.</div>
                 </div>
              </div>
              <div className='Quali'>
              <div className='courseHeading'>High School</div>
                 <div className='college'>
-                <img src={logo} alt="collegeLogo" className="collegeLogo"/>
+                <img src={dgs} alt="collegeLogo" className="collegeLogo"/>
                 <div className='collegeData'>Doon Global School (2019-20)<br/>I completed my High School studies in year 2020 and got 92%.</div>
                 </div>
              </div>
