fdb0c0ea3fc821b31d755676cb9a57ed3929771c
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-17 12:35:56 +0530

    done orientation changes in about me and typewriter

--- Projects/portfolio/src/components/aboutMe/aboutMe.jsx
+++ Projects/portfolio/src/components/aboutMe/aboutMe.jsx
@@ -1,19 +1,20 @@
-import { useTypewriter,Cursor } from "react-simple-typewriter";
-import  './aboutMe.css'
-export const AboutMe=()=>{
-    const [text] = useTypewriter({
-      words: ['Iam Shiwang Bisht'],
-      loop: 1,
-      typeSpeed:100
-    })
-return(
-    <div className="aboutMe">Hi there! <br/>
-    <span className="myName">
-        {text}
-    </span>
-    <Cursor/>
+import { useTypewriter, Cursor } from "react-simple-typewriter";
+import "./aboutMe.css";
+export const AboutMe = () => {
+  const [text] = useTypewriter({
+    words: ["Iam Shiwang Bisht"],
+    loop: 1,
+    typeSpeed: 100,
+  });
+  return (
+    <div>
+      <div className="aboutMe">
+        Hi there! <br />
+        <span className="myName">{text}</span>
+        <Cursor />
+      </div>
     </div>
-)
-}
+  );
+};
 
 export default AboutMe;
