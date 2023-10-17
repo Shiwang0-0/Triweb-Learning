ccd0fcc12c0fcdb456838414e712f11c06d8a564
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-17 14:42:01 +0530

    imported projects,qualification,resume,contact section

--- Projects/portfolio/src/App.js
+++ Projects/portfolio/src/App.js
@@ -2,6 +2,11 @@ import Navbar from "./components/navbar/navbar";
 import AboutMe from "./components/aboutMe/aboutMe";
 import { Skills } from "./components/skills/skills";
 import { Wave } from "./components/wave/wave";
+import { Qualifications } from "./components/qualifications/qualifications";
+import { Projects } from "./components/projects/project";
+import { Resume } from "./components/resume/resume";
+import { Contact } from "./components/contact/contact";
+
 function App() {
   return (
     <div className="App">
@@ -9,6 +14,10 @@ function App() {
       <AboutMe/>
       <Wave/>
       <Skills/>
+      <Qualifications/>
+      <Projects/>
+      <Resume/>
+      <Contact/>
     </div>
   );
 }
