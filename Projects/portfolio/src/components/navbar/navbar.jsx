3b053511c2b0fb8994218b191c0c6d0ac96f9c38
Shiwang0-0 <bisht0shiwang@gmail.com>
2023-10-24 18:01:43 +0530

    added links to social media platforms

--- Projects/portfolio/src/components/navbar/navbar.jsx
+++ Projects/portfolio/src/components/navbar/navbar.jsx
@@ -1,30 +1,50 @@
 import React from "react";
-import  './navbar.css'
+import "./navbar.css";
+import logo from "../../assets/logo.png";
+import github from "../../assets/pngwing.com (12).png";
+import linkedin from "../../assets/pngwing.com (7).png";
+import X from "../../assets/pngwing.com (6).png";
 
-import logo from "../../assets/logo.png"
-import github from '../../assets/pngwing.com (12).png'
-import linkedin from '../../assets/pngwing.com (7).png'
-import X from '../../assets/pngwing.com (6).png'
+const Navbar = () => {
+  const handleReloadClick = () => {
+    window.location.reload();
+    window.scrollTo(0, 0);
+  };
 
-
-const Navbar=()=>{
-    const handleReloadClick=()=>{
-        window.location.reload();
-        window.scrollTo(0, 0);
-    }
-    return (
+  return (
     <nav className="navbar">
-        <img src={logo} onClick={handleReloadClick} alt="Logo" className="logo"/>
-        <div className="socialHandles">
-        {/* <Link  className="socialHandlesLink">Linkedin</Link>
-        <Link className="socialHandlesLink">Twitter</Link>
-        <Link className="socialHandlesLink">Github</Link> */}
-        <img src={linkedin} alt="" className="socialHandlesLink" />
-        <img src={X} alt="" className="socialHandlesLink" />
-        <img src={github} alt="" className="socialHandlesLink" />
-        </div>
-    </nav>
-    )
-}
+      <img src={logo} onClick={handleReloadClick} alt="Logo" className="logo" />
+      <div className="socialHandles">
+        <a
+          href="https://www.linkedin.com/in/shiwang-bisht-b5918b269/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={linkedin} alt="" className="socialHandlesLink" />
+        </a>
+        <a
+          href="https://twitter.com/Shiwang0bisht"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={X} alt="" className="socialHandlesLink" target="_blank" />
+        </a>
 
-export default Navbar
+        <a
+          href="https://github.com/Shiwang0-0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img
+            src={github}
+            alt=""
+            className="socialHandlesLink"
+            target="_blank"
+          />
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
